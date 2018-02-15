389e43d5916c61ecbfe9586a941fb5625f8b5790
Muhand Jumah <muhandjumah@gmail.com>
2018-02-14 23:21:39 -0500

    added .env to .gitignore... added index.js and wrote basic server

---
+++ src/index.js
@@ -0,0 +1,60 @@
+//Requires
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+
+//Global variables/objects
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+//Let variables
+let window;
+
+//////////////////////////
+//Events handeling
+//////////////////////////
+app.on('ready',
+  createWindow
+);
+
+app.on('window-all-closed',
+  () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  }
+);
+
+app.on('activate',
+  () => {
+    if (win === null) {
+      createWindow()
+    }
+  }
+);
+
+//////////////////////////
+//Utility methods
+//////////////////////////
+function createWindow(arguments) {
+  window = new BrowserWindow();
+
+  //Load window
+  window.loadURL(url.format({
+    pathname: path.join(__dirname, '../static/index.html'),
+    protocol: 'file',
+    slashes: true
+  }));
+
+  //Enable dev tools, to debug the app
+  // TODO: Enable this only if configuraiton (Enviornmental variables) is set to debug mode/development
+  window.webContents.openDevTools();
+
+  //Garbage collection  if window is closed
+  window.on('closed',
+    (arguments) => {
+      window = null;
+    }
+  );
+
+}
